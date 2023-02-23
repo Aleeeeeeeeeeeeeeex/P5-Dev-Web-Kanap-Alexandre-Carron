4a204522c1b09c64958752e8a98f4d3b0dc0b866
Alexandre <carronalexandre77@gmail.com>
2023-02-23 17:51:53 +0100

    appel api

---
+++ front/produits.js
@@ -0,0 +1,20 @@
+//récupérer les pièces depuis le jscon
+const reponse = await fetch(" http://localhost:3000/api/products");
+const produits = await reponse.json();
+
+const article = produits[0];
+console.log(article);
+
+//On pointe la section dans laquelle on va injecter les article via la class item
+const sectionItems = document.querySelector(".items");
+
+//création d'une section article qu'on va injecter dans la section item
+const sectionArticle = document.createElement("article");
+
+//création d'une balise img dans laquelle on injecte l'image de l'api
+const imageElement = document.createElement("img");
+imageElement.src = article.imageUrl;
+
+//On place la section article dans la section Item puis on place une image dans la section article
+sectionItems.appendChild(sectionArticle);
+sectionArticle.appendChild(imageElement);
