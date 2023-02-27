44d9a01ab7b26a5a0bb5c7bdc6e72492e2527929
Alexandre <carronalexandre77@gmail.com>
2023-02-27 13:11:54 +0100

    fiche produit

---
+++ front/js/ficheProduits.js
@@ -0,0 +1,20 @@
+//récupérer l'id de l'url
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+console.log(id);
+
+//const maficheproduit = fetch(`http://localhost:3000/api/products/${id}`);
+
+async function fetchData() {
+  const response = await fetch(`http://localhost:3000/api/products/${id}`);
+  const maficheproduit = await response.json();
+  console.log(maficheproduit);
+  const imageElement = document.querySelector(".item__img");
+  const image = document.createElement("img");
+  image.src = maficheproduit.imageUrl;
+  imageElement.appendChild(image);
+  console.log(imageElement);
+  const nomProduit = document.querySelector("#title");
+  nomProduit.textContent = maficheproduit.name;
+}
+fetchData();
